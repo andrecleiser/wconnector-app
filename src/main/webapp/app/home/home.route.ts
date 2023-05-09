8096431ff96d1429aa8fd50e0d26c42c93cb787c
Andre Cleiser <andre.cleiser@polarising.com>
2023-05-09 18:11:08 +0100

    [ENTREGA-4] Corrigido o comportamento quando não tem o perfil ACESSWCON

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -1,11 +1,16 @@
 import { Route } from '@angular/router';
 
 import { HomeComponent } from './home.component';
+import { Authority } from '../config/authority.constants';
+import { UserRouteAccessService } from '../core/auth/user-route-access.service';
 
 export const HOME_ROUTE: Route = {
   path: '',
-  component: HomeComponent,
   data: {
     pageTitle: 'home.title',
+    moduloErro: 'o Desbloqueio de Pedidos',
+    authorities: [Authority.ACESSWCON],
   },
+  canActivate: [UserRouteAccessService],
+  component: HomeComponent,
 };
